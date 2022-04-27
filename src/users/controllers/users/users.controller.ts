47241cf65db12124c50773a1986557e867610c55
Denis Shkabenkov <denelvis@yandex.ru>
2022-04-27 12:29:27 +0300

    added users controllers

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateUserDto } from "./../../dtos/CreateUser.dto";
+import { UsersService } from "../../../users/services/users/users.service";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @Get()
+  getAll() {
+    return this.userService.getAllUsers();
+  }
+
+  @Post()
+  createUser(@Body() userDto: CreateUserDto) {
+    return this.userService.createUser(userDto);
+  }
+}
