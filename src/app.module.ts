46ace2544ccb7336234ca1facf103f2588804e22
Denis Shkabenkov <denelvis@yandex.ru>
2022-04-27 11:19:44 +0300

    added user model, functions to user service and user create dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { UsersModule } from "./users/users.module";
+import { User } from "./users/users.model";
 
 @Module({
   controllers: [],
@@ -15,7 +16,7 @@ import { UsersModule } from "./users/users.module";
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [],
+      models: [User],
       autoLoadModels: true,
     }),
     UsersModule,
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
 import { UsersController } from "./controllers/users/users.controller";
 import { UsersService } from "./services/users/users.service";
-
+import { User } from "./users.model";
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [SequelizeModule.forFeature([User])],
 })
 export class UsersModule {}
